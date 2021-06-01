94cf06e70bf2987cc6fdac817d52b58fa0452f88
Yuzuru <71698842+ItzYuzuruYT@users.noreply.github.com>
2021-06-01 17:43:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    Anime: {
+        AnimeInfo: require('./src/AnimeInfo.js'),
+        AnimeInfoFetch: require('./src/AnimeInfoFetch.js'),
+        GetInfo: require('./src/GetInfo.js'),
+        GetEpisodes: require('./src/GetEpisodes.js')
+    }
+}
