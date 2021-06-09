274b64db17c010dbfbfd48104f270d1d816b0e93
Yuzuru <71698842+ItzYuzuruYT@users.noreply.github.com>
2021-06-09 17:36:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 module.exports = {
-    Anime: {
-        AnimeInfo: require('./src/AnimeInfo.js'),
-        AnimeInfoFetch: require('./src/AnimeInfoFetch.js'),
-        GetInfo: require('./src/GetInfo.js'),
-        GetEpisodes: require('./src/GetEpisodes.js')
-    }
+    Anime: require('./src/Anime.js')
 }
